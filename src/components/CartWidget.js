e46ae8aa756e91b6934870f922f2a2aa0abad84f
Lautaro <lautaroacosta@MacBook-Pro-de-Lautaro.local>
2021-11-15 23:06:54 -0300

    subo

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'; 
 import { Link } from 'react-router-dom';
 import '../css/cartwidget.css'
+import { CartContext } from './context/CartContext';
 
 export const CartWidget = () => {
-    
+    const {cartList} = useContext(CartContext)
     
     return (
-        <Link to="/cart">
-            <AiOutlineShoppingCart className='cartWidget' />
-        </Link>
+        <div>
+            {cartList.length
+            ? <div>
+            <Link to="/cart">
+                <AiOutlineShoppingCart className='cartWidget' />
+            </Link>
+        </div>
+            : ''
+        }
+        </div>
+        
+        
     )
 }
 
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -32,6 +32,5 @@ export const Cart = () => {
           ))}
         </div>
       )
-   
     
 }
